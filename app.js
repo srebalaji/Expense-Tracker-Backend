8033fc1a1af0df6026e1389f99e9b44bf183f377
srebalaji <srebalaji@outlook.com>
2017-06-19 14:31:44 +0530

    Connecting mongodb and initiating monk
    1. Connecting monogdb and initiating monogodb Object.
    2. Initiating monk ORM.

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongodb = require('mongodb');
+var ObjectID = require('mongodb').ObjectID;
+var monk = require('monk');
+var db = monk('localhost:27017/sample_tracker_testing');
 var users = require('./routes/users');
 
 var app = express();

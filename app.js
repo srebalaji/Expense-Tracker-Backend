c3af8bbd0473fcbf1db82ab6789e599a060c1dce
srebalaji <srebalaji@outlook.com>
2017-06-19 14:34:45 +0530

    Request and response headers
    1. Adding response headers to the app.
    2. Passing db and its object to the router.

--- app.js
+++ app.js
@@ -12,7 +12,18 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(cookieParser());
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+  req.db = db;
+  req.ObjectID = ObjectID;
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use('/api/v1/users', users);
 
